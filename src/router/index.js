e2e995070209671846e633a64ea2dbdab800fd7b
James <james.ryan.mcguire@gmail.com>
2021-07-26 15:30:06 -0700

    Adds logout page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Signup from "../views/Signup.vue";
+import Logout from "../views/Logout.vue";
 import ProductIndex from "../views/ProductsIndex.vue";
 
 Vue.use(VueRouter);
@@ -22,16 +23,9 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   { path: "/login", name: "login", component: Login },
-  {
-    path: "/signup",
-    name: "Signup",
-    component: Signup,
-  },
-  {
-    path: "/products",
-    name: "products-index",
-    component: ProductIndex,
-  },
+  { path: "/signup", name: "Signup", component: Signup },
+  { path: "/logout", name: "logout", component: Logout },
+  { path: "/products", name: "products-index", component: ProductIndex },
 ];
 
 const router = new VueRouter({
