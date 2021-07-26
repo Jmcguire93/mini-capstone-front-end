0e024c26f4f2bf0754483f8c5790d276ed3d9239
James <james.ryan.mcguire@gmail.com>
2021-07-26 15:14:59 -0700

    Adds ProductsIndex path to home

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -2,12 +2,12 @@
   <div class="products-index">
     <h1>{{ message }}</h1>
 
-    <div v-for="post in products" :key="post.id">
-      <h2>Title: {{ post.title }}</h2>
-      <p>Body: {{ post.body }}</p>
-      <img v-bind:src="post.image" alt="post.title" />
-      <!-- <p><router-link to="`/show/${post.id}`">Link to this post</router-link></p> -->
-      <p><router-link v-bind:to="`/products/${post.id}`">Link to post</router-link></p>
+    <div v-for="product in products" :key="product.id">
+      <h2>Name: {{ product.name }}</h2>
+      <p>Description: {{ product.desscription }}</p>
+      <img v-bind:src="product.image" alt="product.name" />
+
+      <p><router-link v-bind:to="`/products/${product.id}`">Link to product</router-link></p>
     </div>
   </div>
 </template>
